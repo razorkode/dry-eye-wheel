0d8e16f3ea68cfea4c780e3616795ff3ea9dd376
Philip Theobald <fylzero@gmail.com>
2023-04-03 11:21:11 -0500

    Setup first animation test with multiple ids and position targeting

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import SunAnimation from './components/SunAnimation.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <table style="border: 2px solid red">
+        <tr>
+            <td><SunAnimation targetId="sunAnim1" /></td>
+            <td><SunAnimation targetId="sunAnim2" /></td>
+        </tr>
+        <tr>
+            <td><SunAnimation targetId="sunAnim3" /></td>
+            <td><SunAnimation targetId="sunAnim4" /></td>
+        </tr>
+    </table>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
