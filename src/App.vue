5aa15a69d193cf8e3d3de6b8219aea985abf8218
Philip Theobald <fylzero@gmail.com>
2023-04-27 17:28:28 -0500

    Restructured to SVG as Vue component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup>
 import SunAnimation from './components/icons/SunAnimation.vue';
+import WheelSvg from './components/WheelSvg.vue';
 </script>
 
 <template>
-    <span>No Font Applied</span>
-    <br />
-    <span class="font-euclid-circular-bold">EuclidCircularA BOLD</span>
-    <br />
-    <span class="font-euclid-circular-semibold">EuclidCircularA SEMIBOLD</span>
-    <img
-        src="./assets/wheel.svg"
-        alt="" />
-    <table style="border: 2px solid red">
+    <WheelSvg />
+
+    <table>
         <tr>
             <td><SunAnimation targetId="sunAnim1" /></td>
             <td><SunAnimation targetId="sunAnim2" /></td>
---
+++ src/components/wheel-atoms/ring4-outer/TextMeasurement.vue
@@ -0,0 +1,17 @@
+<template>
+    <path
+        id="SVGID_x5F_00000091724619102403552530000000835034987211212681_x5F_"
+        class="fill-none"
+        d="M786.9,190 C929.4,369,899.9,629.6,721,772.1s-439.5,113-582.1-65.9s-113-439.5,65.9-582.1S644.3,11.1,786.9,190z" />
+    <text>
+        <textPath
+            xlink:href="#SVGID_x5F_00000091724619102403552530000000835034987211212681_x5F_"
+            startOffset="69.7667%">
+            <tspan
+                class="font-euclid-circular-bold"
+                style="fill: #a6a8ac; font-size: 49.7131px; letter-spacing: 75">
+                MEASUREMENT
+            </tspan>
+        </textPath>
+    </text>
+</template>
