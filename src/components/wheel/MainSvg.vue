a142174a29710944c8bb22c29513d91adb41debf
Philip Theobald <fylzero@gmail.com>
2023-04-28 14:34:31 -0500

    Added animations

--- src/components/wheel/MainSvg.vue
+++ src/components/wheel/MainSvg.vue
@@ -9,7 +9,7 @@ const controlsStore = useControlsStore();
 
 const setContainerSize = () => {
     const container = document.querySelector('.container');
-    const firstSvg = container.querySelector('.first-svg');
+    const firstSvg = container.querySelector('.mitigation-svg');
 
     const width = firstSvg.clientWidth || firstSvg.getBoundingClientRect().width;
     const height = firstSvg.clientHeight || firstSvg.getBoundingClientRect().height;
@@ -29,26 +29,122 @@ onMounted(() => {
         <div class="container">
             <MitigationSvg
                 :class="{
-                    'left-[-40px] top-[-20px]': controlsStore.selectedArea === 'mitigation',
+                    active: controlsStore.selectedArea === 'mitigation',
                     grayscale: controlsStore.selectedArea !== 'mitigation',
                 }"
-                class="first-svg absolute max-h-[500px] w-full" />
+                class="mitigation-svg absolute max-h-[500px] w-full transition-all duration-300" />
 
             <MeasurementSvg
                 :class="{
-                    'left-[40px] top-[-20px]': controlsStore.selectedArea === 'measurement',
+                    active: controlsStore.selectedArea === 'measurement',
                     grayscale: controlsStore.selectedArea !== 'measurement',
                 }"
-                class="absolute max-h-[500px] w-full" />
+                class="measurement-svg absolute max-h-[500px] w-full transition-all duration-300" />
 
             <ManagementSvg
                 :class="{
-                    'top-[45px]': controlsStore.selectedArea === 'management',
+                    active: controlsStore.selectedArea === 'management',
                     grayscale: controlsStore.selectedArea !== 'management',
                 }"
-                class="absolute max-h-[500px] w-full" />
+                class="management-svg absolute max-h-[500px] w-full" />
         </div>
     </div>
 </template>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+.mitigation-svg {
+    top: 0px;
+    left: 0px;
+    animation: mitigation-slide-in 300ms ease-in-out 1 alternate;
+
+    &.active {
+        top: -20px;
+        left: -40px;
+        animation: mitigation-slide-out 300ms ease-in-out 1 alternate;
+    }
+}
+
+@keyframes mitigation-slide-in {
+    0% {
+        top: -20px;
+        left: -40px;
+    }
+    100% {
+        top: 0px;
+        left: 0px;
+    }
+}
+
+@keyframes mitigation-slide-out {
+    0% {
+        top: 0px;
+        left: 0px;
+    }
+    100% {
+        top: -20px;
+        left: -40px;
+    }
+}
+
+.measurement-svg {
+    top: 0px;
+    left: 0px;
+    animation: measurement-slide-in 300ms ease-in-out 1 alternate;
+
+    &.active {
+        top: -20px;
+        left: 40px;
+        animation: measurement-slide-out 300ms ease-in-out 1 alternate;
+    }
+}
+
+@keyframes measurement-slide-in {
+    0% {
+        top: -20px;
+        left: 40px;
+    }
+    100% {
+        top: 0px;
+        left: 0px;
+    }
+}
+
+@keyframes measurement-slide-out {
+    0% {
+        top: 0px;
+        left: 0px;
+    }
+    100% {
+        top: -20px;
+        left: 40px;
+    }
+}
+
+.management-svg {
+    top: 0px;
+    animation: management-slide-in 300ms ease-in-out 1 alternate;
+
+    &.active {
+        top: 45px;
+        animation: management-slide-out 300ms ease-in-out 1 alternate;
+    }
+}
+
+@keyframes management-slide-in {
+    0% {
+        top: 45px;
+    }
+    100% {
+        top: 0px;
+    }
+}
+
+@keyframes management-slide-out {
+    0% {
+        top: 0px;
+    }
+    100% {
+        top: 45px;
+    }
+}
+</style>
