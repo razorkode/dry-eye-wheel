193873ec1eaf719ad64bcca5b90c2d8121e97203
Philip Theobald <fylzero@gmail.com>
2023-04-29 19:33:24 -0500

    Broke test output to component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { useControlsStore } from './stores/controlsStore.js';
-import SunAnimation from './components/icons/SunAnimation.vue';
-import WheelSvg from './components/wheel/MainSvg.vue';
-import VerticalSlider from './components/VerticalSlider.vue';
-import Card from './components/Card.vue';
-import ButtonMenu from './components/layout/ButtonMenu.vue';
+import { useControlsStore } from '@/stores/controlsStore.js';
+import SunAnimation from '@/components/icons/SunAnimation.vue';
+import WheelSvg from '@/components/wheel/MainSvg.vue';
+import VerticalSlider from '@/components/VerticalSlider.vue';
+import Card from '@/components/Card.vue';
+import ButtonMenu from '@/components/layout/ButtonMenu.vue';
+import TestOutput from '@/components/TestOutput.vue';
 
 const controlsStore = useControlsStore();
 </script>
@@ -13,55 +14,12 @@ const controlsStore = useControlsStore();
     <div class="p-6">
         <div class="flex h-[640px]">
             <VerticalSlider />
-
             <ButtonMenu />
 
             <!-- Wheel Area -->
             <div class="h-full w-full">
                 <WheelSvg />
-
-                <!-- Test Output -->
-                <div
-                    class="absolute right-2 top-1 rounded-lg bg-slate-950 bg-opacity-60 px-5 py-3 font-mono text-sm leading-5 text-white">
-                    <table>
-                        <tr>
-                            <td class="pr-2 text-gray-300">Default State</td>
-                            <td>{{ !!controlsStore.defaultState }}</td>
-                        </tr>
-                        <tr>
-                            <td class="pr-2 text-gray-300">Slider</td>
-                            <td>{{ controlsStore.sliderValue }}</td>
-                        </tr>
-                        <tr>
-                            <td class="pr-2 text-gray-300">Area</td>
-                            <td>{{ controlsStore.selectedArea }}</td>
-                        </tr>
-                        <tr>
-                            <td class="pr-2 text-gray-300">Section</td>
-                            <td>{{ controlsStore.selectedSection }}</td>
-                        </tr>
-                        <tr>
-                            <td class="pr-2 text-gray-300">Item</td>
-                            <td>{{ controlsStore.selectedItem }}</td>
-                        </tr>
-                        <tr>
-                            <td class="pr-2 text-gray-300">triagingSectionClasses</td>
-                            <td>{{ controlsStore.triagingSectionClasses }}</td>
-                        </tr>
-                        <tr>
-                            <td class="pr-2 text-gray-300">riskFactorsSectionClasses</td>
-                            <td>{{ controlsStore.riskFactorsSectionClasses }}</td>
-                        </tr>
-                        <tr>
-                            <td class="pr-2 text-gray-300">diagnosisSectionClasses</td>
-                            <td>{{ controlsStore.diagnosisSectionClasses }}</td>
-                        </tr>
-                        <tr>
-                            <td class="pr-2 text-gray-300">subClassificationSectionClasses</td>
-                            <td>{{ controlsStore.subClassificationSectionClasses }}</td>
-                        </tr>
-                    </table>
-                </div>
+                <!-- <TestOutput /> -->
             </div>
         </div>
 
---
+++ src/components/TestOutput.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { useControlsStore } from '@/stores/controlsStore.js';
+const controlsStore = useControlsStore();
+</script>
+
+<template>
+    <div
+        class="absolute right-2 top-1 rounded-lg bg-slate-950 bg-opacity-60 px-5 py-3 font-mono text-sm leading-5 text-white">
+        <table>
+            <tr>
+                <td class="pr-2 text-gray-300">Default State</td>
+                <td>{{ !!controlsStore.defaultState }}</td>
+            </tr>
+            <tr>
+                <td class="pr-2 text-gray-300">Slider</td>
+                <td>{{ controlsStore.sliderValue }}</td>
+            </tr>
+            <tr>
+                <td class="pr-2 text-gray-300">Area</td>
+                <td>{{ controlsStore.selectedArea }}</td>
+            </tr>
+            <tr>
+                <td class="pr-2 text-gray-300">Section</td>
+                <td>{{ controlsStore.selectedSection }}</td>
+            </tr>
+            <tr>
+                <td class="pr-2 text-gray-300">Item</td>
+                <td>{{ controlsStore.selectedItem }}</td>
+            </tr>
+            <tr>
+                <td class="pr-2 text-gray-300">triagingSectionClasses</td>
+                <td>{{ controlsStore.triagingSectionClasses }}</td>
+            </tr>
+            <tr>
+                <td class="pr-2 text-gray-300">riskFactorsSectionClasses</td>
+                <td>{{ controlsStore.riskFactorsSectionClasses }}</td>
+            </tr>
+            <tr>
+                <td class="pr-2 text-gray-300">diagnosisSectionClasses</td>
+                <td>{{ controlsStore.diagnosisSectionClasses }}</td>
+            </tr>
+            <tr>
+                <td class="pr-2 text-gray-300">subClassificationSectionClasses</td>
+                <td>{{ controlsStore.subClassificationSectionClasses }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<style lang="postcss" scoped></style>
