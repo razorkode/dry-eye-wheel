7699fa4a278cea786ed8527f656738f4b494a28a
Philip Theobald <fylzero@gmail.com>
2023-04-30 00:39:24 -0500

    Added animation to popover

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+
 const props = defineProps({
     title: {
         type: String,
@@ -9,10 +11,20 @@ const props = defineProps({
         default: 'popup',
     },
 });
+
+const showCard = ref(false);
+
+onMounted(() => {
+    setTimeout(() => {
+        showCard.value = true;
+    }, 100);
+});
 </script>
 
 <template>
-    <div class="flex max-w-md flex-col gap-2 rounded-2xl border border-gray-400 bg-white p-6 shadow-md">
+    <div
+        :class="[showCard ? ' translate-y-0 opacity-100' : 'translate-y-5 opacity-0']"
+        class="flex max-w-md flex-col gap-2 rounded-2xl border border-gray-400 bg-white p-6 shadow-lg transition-all duration-700">
         <div
             v-if="$slots.icon"
             class="text-center">
